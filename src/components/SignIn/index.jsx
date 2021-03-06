aaf969df1aee6aba396f83b832c44cb0673854ab
Esa Mäkipää <esa.makipaa@gmail.com>
2021-03-06 09:50:53 +0200

    Exercises 10.14 - 10.15 done, storing the access token

--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from "react-router-native";
 import { Formik } from 'formik';
 import * as yup from 'yup';
 import SignInForm from './SignInForm';
@@ -23,13 +24,15 @@ const validationSchema = yup.object().shape({
 const SignIn = () => {
   const [signIn] = useSignIn();
 
+  const history = useHistory();
+
   const onSubmit = async (values) => {
     const { username, password } = values;
 
     try {
-      const { data } = await signIn({ username, password });
+      const data = await signIn({ username, password });
       console.log(data);
-
+      history.push('/');
     } catch (e) {
       console.log(e);
     }
